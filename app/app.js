37844534a5408b2a6bd24d722a19e78609bc2667
David Helgeson <davidhelgeson@Davids-MacBook-Pro-2.local>
2016-05-15 19:34:10 -0700

    init w/ browser time, tron styling

--- app/app.js
+++ app/app.js
@@ -6,28 +6,37 @@ app.time = {
   'seconds': null
 };
 
-app.run = function(){
-  var _self = this;
-  window.setInterval(function(){
-    window.requestAnimationFrame(_self.main_loop)
-  }, 17);
+app.rotate = function(id, degrees) {
+  console.log(id, degrees);
+  var el = document.getElementById( id );
+  console.log(el);
+  el.style.transform = 'rotate(' + degrees + 'deg)';
 };
 
 app.main_loop = function(){
-  // 
-  // do things
-  // 
+
+  var _self          = app;
+  var date           = new Date();
   
-  var date          = new Date();
+  _self.time.seconds = date.getSeconds();
+  _self.time.minutes = date.getMinutes();
+  _self.time.hours   = date.getHours();
+  
+  var secondPosition = _self.time.seconds * 6;
+  var minutePosition = _self.time.minutes * 6;
+  var hourPosition   = _self.time.hours * 30 + ( _self.time.minutes / 2 );
 
-  this.time.seconds = date.getSeconds();
-  this.time.minutes = date.getMinutes();
-  this.time.hours   = function(){
-    var _hours = date.getHours();
-    if(_hours > 12){
-      return hours - 12;
-    }
-  }();
+  _self.rotate('seconds', secondPosition);
+  _self.rotate('minutes', minutePosition);
+  _self.rotate('hours', hourPosition);
 
-}
+  // requestAnimationFrame( app.main_loop );
 
+};
+
+app.run = function(){
+  var _self = app;
+  window.setTimeout(function(){
+    _self.main_loop();
+  },0);
+}();
